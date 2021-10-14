800d5910d124371dfcc2b0300f8a17d3a21f59ec
FabioCavallero <f.cavallero.1286@vallauri.edu>
2021-10-14 11:31:54 +0200

    ngFor directive and input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '5 INF B 2021-22';
+  title = '5 INF B 2021/22';
+  studentList=[
+    {name:'GianRenato Marchisio', hobby: 'Yoga', gender:'M', isPro:true},
+    {name:'Andrea Vergano', hobby: 'IQOS', gender:'M', isPro:true},
+    {name:'Aaaaliscee Barenghi', hobby: 'Volley', gender:'F', isPro:false},
+    {name:'Relo SulRello', hobby: 'Atletica', gender:'M', isPro:false},
+    {name:'Zeta Kappa', hobby: 'Calcio', gender:'M', isPro:true},
+    {name:'Benedetta Parodi', hobby: 'Volley', gender:'F', isPro:false},
+    {name:'GianLearco Lombardi', hobby: 'Yoga', gender:'M', isPro:true},
+    {name:'GianRoberto Mecaj', hobby: 'Calcio', gender:'M', isPro:true},
+    {name:'Panero Fabio', hobby: 'IQOS', gender:'M', isPro:false},
+    {name:'Dedde Vaschetto', hobby: 'Calcio', gender:'M', isPro:false},
+    {name:'Marco Gillone', hobby: 'Atletica', gender:'M', isPro:false},
+    {name:'Margot Conlat', hobby: 'Volley', gender:'F', isPro:false}
+  ]
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,34 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  studentList=[
-    {name:'GianRenato Marchisio', hobby: 'Yoga', gender:'M', isPro:true},
-    {name:'Andrea Vergano', hobby: 'IQOS', gender:'M', isPro:true},
-    {name:'Aaaaliscee Barenghi', hobby: 'Volley', gender:'F', isPro:false},
-    {name:'Relo SulRello', hobby: 'Atletica', gender:'M', isPro:false},
-    {name:'Zeta Kappa', hobby: 'Calcio', gender:'M', isPro:true},
-    {name:'Benedetta Parodi', hobby: 'Volley', gender:'F', isPro:false},
-    {name:'GianLearco Lombardi', hobby: 'Yoga', gender:'M', isPro:true},
-    {name:'GianRoberto Mecaj', hobby: 'Calcio', gender:'M', isPro:true},
-    {name:'Panero Fabio', hobby: 'IQOS', gender:'M', isPro:false},
-    {name:'Dedde Vaschetto', hobby: 'Calcio', gender:'M', isPro:false},
-    {name:'Marco Gillone', hobby: 'Atletica', gender:'M', isPro:false},
-    {name:'Margot Conlat', hobby: 'Volley', gender:'F', isPro:false}
-  ]
-  student:any;
-  constructor() { 
-    this.randomStudent();
+@Input()student:any;
+  constructor() {}
+  ngOnInit(): void {
+    this.randomStudentPro();
   }
-  private randomStudent(){
-    let num=Math.floor(Math.random()*this.studentList.length);
-    this.student=this.studentList[num];
-  }
-  ngOnInit(): void {}
   onStudentClick(){
-    this.randomStudent();
+    this.student.isPro=!this.student.isPro;
+  }
+  private randomStudentPro(){
+    let num=Math.floor(Math.random()*2);
+    this.student.isPro=(num==1);
   }
 }
